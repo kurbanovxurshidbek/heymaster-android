30dfbbe4cc61a9758a61f76f25b5e40a92a2da08
Shahriyor <shahriyorabdullayev2000@gmail.com>
2022-05-16 12:05:02 +0500

    test

--- app/src/main/java/com/heymaster/heymaster/MainActivity.kt
+++ app/src/main/java/com/heymaster/heymaster/MainActivity.kt
@@ -2,10 +2,13 @@ package com.heymaster.heymaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("@@@", "Test")
     }
 }
