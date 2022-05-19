76757810389c7ba9804ea5bab14ebcc8f464e926
Shahriyor <shahriyorabdullayev2000@gmail.com>
2022-05-19 20:13:53 +0500

    added some code

--- app/src/main/java/com/heymaster/heymaster/ui/SplashActivity.kt
+++ app/src/main/java/com/heymaster/heymaster/ui/SplashActivity.kt
@@ -1,18 +1,45 @@
 package com.heymaster.heymaster.ui
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.heymaster.heymaster.R
 import com.heymaster.heymaster.databinding.ActivitySplashBinding
+import com.heymaster.heymaster.ui.auth.LoginActivity
+import com.heymaster.heymaster.ui.global.BaseActivity
+import com.heymaster.heymaster.ui.intro.IntroActivity
+
 
 @SuppressLint("CustomSplashScreen")
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
 
     private val binding by lazy { ActivitySplashBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        countTimer()
+
+
     }
+
+    private fun countTimer() {
+        object : CountDownTimer(2000, 1000) {
+            override fun onTick(p0: Long) {}
+
+            override fun onFinish() {
+                callIntroActivity()
+            }
+
+        }.start()
+    }
+
+
+
+
+
+
 }
---
+++ app/src/main/java/com/heymaster/heymaster/ui/master/fragment/home/MasterSearchFragment.kt
@@ -0,0 +1,14 @@
+package com.heymaster.heymaster.ui.master.fragment.home
+
+import android.os.Bundle
+import android.view.View
+import com.heymaster.heymaster.R
+import com.heymaster.heymaster.ui.global.BaseFragment
+
+class MasterSearchFragment: BaseFragment(R.layout.fragment_search) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
