7e68c5d0cdd276002fd84f2e7b33262bd1d5ffc3
FeruzDavlatoV <marketolog.mate.max@gmail.com>
2022-05-24 18:29:55 +0500

    added some functions

--- app/src/main/java/com/heymaster/heymaster/utils/Constants.kt
+++ app/src/main/java/com/heymaster/heymaster/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.heymaster.heymaster.utils
 
 object Constants {
 
-//    val BASE_URL = "https://628a284ce5e5a9ad322183ca.mockapi.io"
-    val BASE_URL = "https://628b684f667aea3a3e2eae0d.mockapi.io/api/"
+    val BASE_URL = "https://628a284ce5e5a9ad322183ca.mockapi.io"
+//    val BASE_URL = "https://628b684f667aea3a3e2eae0d.mockapi.io/api/"
 }
